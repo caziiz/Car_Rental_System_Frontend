8ea90ae3937408af4edcfc2f2b70e7508462fee6
Abdiaziz <casiisc608@gmail.com>
2026-07-04 22:18:22 +0300

    fixed registration

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
-import register from "./Pages/Register";
-
+import Register from "./Register.jsx";
 function Login() {
   const navigate = useNavigate();
   const { user, login } = useAuth();
